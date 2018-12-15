277351f9038c0f8de8a87fb98d7ade215221261d
casper <casper@spook>
2018-12-15 16:35:14 +0300

    Döngüye Güncellendi

--- DortIslem/src/mustafa/base/DortIslem.java
+++ DortIslem/src/mustafa/base/DortIslem.java
@@ -4,26 +4,34 @@ import java.util.Scanner;
 
 public class DortIslem {
 	
+	
+	private boolean kontrol;
 	private Scanner input;
 	private int sayi1,sayi2,sonuc;
-	private	String islem;
+	private	String islem , cikis;
 	
 	public DortIslem() {
 		// TODO Auto-generated constructor stub
+		this.kontrol = true;
 		this.input = new Scanner(System.in);
-		sayiAl();
+		while(kontrol) {
+			sayiAl();
+			System.out.print("Cikmak icin 'x' e devam etmek icin bir tusa basin: ");
+			this.cikis = this.input.next();
+			if(this.cikis.toUpperCase().equals("X"))
+				this.kontrol = false;
+		}	
 		input.close();
 	}
-	
 	public void sayiAl() {
 
-		System.out.print("Lütfen ilk sayýyý giriniz: ");
+		System.out.print("Lutfen ilk sayiyi giriniz: ");
 		this.sayi1 = this.input.nextInt();
 		
-		System.out.print("Lütfen ikinci sayýyý giriniz: ");
+		System.out.print("Lutfen ikinci sayiyi giriniz: ");
 		this.sayi2 = this.input.nextInt();
 		
-		System.out.print("Lütfen iþlem sembolünü giriniz(+,-,/,*): ");
+		System.out.print("Lutfen islem sembolunu giriniz(+,-,/,*): ");
 		this.islem = this.input.next();
 
 		if(this.islem.equals("+"))
@@ -35,7 +43,7 @@ public class DortIslem {
 		else if(this.islem.equals("/"))
 			bolme();
 		else
-			System.out.println("hatalý bir iþlem girdiniz.");
+			System.out.println("hatalï¿½ bir iï¿½lem girdiniz.");
 	}
 	
 	private void toplama() {
@@ -54,16 +62,19 @@ public class DortIslem {
 	}
 	
 	private void bolme() {
-		this.sonuc = this.sayi1 / this.sayi2;
-		toPrint();
+		if(this.sayi2 == 0)
+			System.out.println("Sifira bolme hatasi");
+		else {
+			this.sonuc = this.sayi1 / this.sayi2;
+			toPrint();
+		}
 	}
 	
 	
 	private void toPrint() {
 		
-		System.out.println("giriþ baþarýlý ");
-		System.out.println("ilk sayý = " + this.sayi1);
-		System.out.println("Ýkinci sayý = " + this.sayi2);
+		System.out.println("ilk sayi = " + this.sayi1);
+		System.out.println("Ikinci sayi = " + this.sayi2);
 		System.out.println(this.sayi1 + " " + this.islem + " " + this.sayi2+" = " + this.sonuc);
 	}
 
